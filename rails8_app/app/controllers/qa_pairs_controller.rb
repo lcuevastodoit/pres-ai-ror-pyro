f15bc0dc9419c336a6b64179033166db9d653edb
Luis Cuevas <lcuevas@tirant.com>
2025-06-11 11:40:03 -0500

    Renames controller and adds index action for QA pairs
    Renames the QaPairController to QaPairsController for consistency.
    
    Adds an index action to retrieve all QA pairs, enhancing functionality for listing existing pairs.

--- rails8_app/app/controllers/qa_pairs_controller.rb
+++ rails8_app/app/controllers/qa_pairs_controller.rb
@@ -1,4 +1,8 @@
-class QaPairController < ApplicationController
+class QaPairsController < ApplicationController
+  def index
+    @qa_pairs = QaPair.all
+  end
+
   def create
     @qa_pair = QaPair.create(question: params[:question], answer: params[:answer])
     render partial: "qa_pairs/new", locals: { qa: @qa_pair }
