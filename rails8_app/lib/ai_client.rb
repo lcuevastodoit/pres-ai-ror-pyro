f96a0256882ecebb8607efa0bd2a6f3a2d29ede2
Luis Cuevas <lcuevas@tirant.com>
2025-06-27 22:53:48 -0500

    [fix] Update client path in AiClient class
    - Changed CLIENT_PATH to point to 'client2.py'
    - Ensures correct file is used for AI client operations

--- rails8_app/lib/ai_client.rb
+++ rails8_app/lib/ai_client.rb
@@ -2,7 +2,7 @@ require 'open3'
 
 class AiClient
   PYTHON_PATH = 'python3'
-  CLIENT_PATH = File.expand_path('../client.py', __FILE__)
+  CLIENT_PATH = File.expand_path('../client2.py', __FILE__)
 
   def self.ask(question)
     cmd = [PYTHON_PATH, CLIENT_PATH, question.to_s]
